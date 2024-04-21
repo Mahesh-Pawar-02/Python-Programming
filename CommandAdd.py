15df3dcf9a5a508a688a5e1a61804d9ff809e8de
Mahesh Dinkar Pawar <maheshpawar30627@gmail.com>
2024-04-21 21:24:40 +0530

    Fourth day of python

---
+++ CommandAdd.py
@@ -0,0 +1,11 @@
+import sys
+
+def main():
+
+    print("Demonstration of command line agrguments")
+
+    print("Enter first number : ",int(sys.argv[1])+int(sys.argv[2]))
+
+
+if __name__ == "__main__":
+    main()
