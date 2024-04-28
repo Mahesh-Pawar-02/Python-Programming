9990c6bd95b9673240fa5850aa7846a5180788de
Mahesh Dinkar Pawar <maheshpawar30627@gmail.com>
2024-04-28 22:02:05 +0530

    fifth day of python

--- Addition.py
+++ Addition.py
@@ -1,9 +1,9 @@
-No1 = int(input("Enter First Number : "))
-No2 = int(input("Enter Second Number : "))
+print("Enter first number : ")
+No1 = input()
+
+print("Enter second number : ")
+No2 = input()
 
 Ans = No1 + No2
-
-print(type(No1))
-print(type(No2))
 
 print("Addition is : ",Ans)
